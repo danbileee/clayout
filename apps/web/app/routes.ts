ca8bb678823c9f9d56f6c87aae4b509b45a323d7
Danbi Lee <hello@danbileee.com>
2025-07-16 17:22:04 +0900

    TEST: Add vercel config

--- apps/web/app/routes.ts
+++ apps/web/app/routes.ts
@@ -1,4 +1,11 @@
-import { type RouteConfig } from '@react-router/dev/routes'
-import { flatRoutes } from '@react-router/fs-routes'
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default flatRoutes() satisfies RouteConfig
+export default [
+  index("./routes/_index.tsx"),
+  route("counter", "./routes/counter.tsx"), // dummy route for API test
+  route("login", "./routes/login.tsx"),
+  route("logout", "./routes/logout.tsx"),
+  route("protected", "./routes/protected.tsx"),
+  route("auth/oauth", "./routes/auth.oauth.tsx"),
+  route("auth/error", "./routes/auth.error.tsx"),
+] satisfies RouteConfig;
